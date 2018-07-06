0b77e6dbb2de1082502a52780278e514977e9c0a
Daniel Munday <mundaymeister@gmail.com>
2018-07-06 22:19:47 +0100

    [tic-tac-toe] added game state

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -3,13 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPlayer: 'x',
+      a1: '',
+      a2: '',
+      a3: '',
+      b1: '',
+      b2: '',
+      b3: '',
+      c1: '',
+      c2: '',
+      c3: '',
+    };
+  }
+
+  selectSquare = (square, player) => {
+    this.setState({
+      [square]: player,
+      currentPlayer: player === 'x' ? 'o' : 'x'
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
