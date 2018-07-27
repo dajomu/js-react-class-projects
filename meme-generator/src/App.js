6639d460a8495759857aee8177ff7bb93015b892
Daniel Munday <mundaymeister@gmail.com>
2018-07-27 15:36:47 +0100

    [meme-generator] allow user to choose from unsplash or giphy images

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const config = {
+  giphyApiKey: '6xAd6ZCXVM5RjGTYjT4szscYQUK1ptNM',
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      imageSource: "unsplash",
       memeImage: "https://images.freeimages.com/images/large-previews/7e9/ladybird-1367182.jpg",
       memeText: "Meme Text x",
     }
   }
 
   randomizeContent = async () => {
-    const imageFetch = fetch('https://source.unsplash.com/random/800x600');
-    const textFetch = fetch('https://talaikis.com/api/quotes/random/');
+    const { imageSource } = this.state;
+    const imageFetch = imageSource === 'unsplash' ?
+      fetch('https://source.unsplash.com/random/800x600') :
+      fetch(`http://api.giphy.com/v1/gifs/random?api_key=${config.giphyApiKey}`).then(function(response){ 
+        return response.json()
+      });
+    const textFetch = fetch('https://talaikis.com/api/quotes/random/').then(function(response){ 
+      return response.json()
+    });
     Promise.all([imageFetch, textFetch]).then(([imageResponse, textResponse]) => {
-      textResponse.json().then(
-        textData => {
-          this.setState({memeImage: imageResponse.url, memeText: textData.quote})
-        });
+      const memeImage = imageSource === 'unsplash' ? imageResponse.url : imageResponse.data.image_url;
+      this.setState({memeImage: memeImage, memeText: textResponse.quote})
     }).catch(error => console.log(error))
   }
 
@@ -35,12 +45,16 @@ class App extends Component {
     this.setState({memeImage: event.target.value});
   }
 
-  changeText= (event) => {
+  changeText = (event) => {
     this.setState({memeText: event.target.value});
+  }
+  
+  handleImageSourceChange = (event) => {
+    this.setState({imageSource: event.target.value});
   }
 
   render() {
-    const { memeImage, memeText } = this.state;
+    const { imageSource, memeImage, memeText } = this.state;
     return (
       <div className="App">
         <div className="meme__container">
@@ -63,6 +77,18 @@ class App extends Component {
               name="memeText"
               value={memeText}
               onChange={this.changeText} />
+          </label>
+          <label>
+            <span>Unsplash:</span>
+            <input type="radio" value="unsplash" 
+              checked={imageSource === 'unsplash'} 
+              onChange={this.handleImageSourceChange} />
+          </label>
+          <label>
+            <span>Giphy:</span>
+            <input type="radio" value="giphy" 
+              checked={imageSource === 'giphy'} 
+              onChange={this.handleImageSourceChange} />
           </label>
           <button onClick={this.randomizeContent}>
             Randomize!!
