7a719dd2cf8735a085757935807922c6a81a8ed9
Daniel Munday <mundaymeister@gmail.com>
2018-07-27 12:01:36 +0100

    [meme-generator] v1: basic meme image and text added

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="meme__container">
+          <img className="meme__image" src="https://images.freeimages.com/images/large-previews/7e9/ladybird-1367182.jpg" />
+          <p class="meme__text">Meme Text</p>
+        </div>
       </div>
     );
   }
