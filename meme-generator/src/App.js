5b6ea50527a66c16070e3446ea9a0b154413fb02
Daniel Munday <mundaymeister@gmail.com>
2018-07-27 12:05:21 +0100

    [meme-generator] v1: basic meme image and text stored in state

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      memeImage: "https://images.freeimages.com/images/large-previews/7e9/ladybird-1367182.jpg",
+      memeText: "Meme Text",
+    }
+  }
+
   render() {
+    const { memeImage, memeText } = this.state;
     return (
       <div className="App">
         <div className="meme__container">
-          <img className="meme__image" src="https://images.freeimages.com/images/large-previews/7e9/ladybird-1367182.jpg" />
-          <p class="meme__text">Meme Text</p>
+          <img className="meme__image" src={memeImage}/>
+          <p class="meme__text">{memeText}</p>
         </div>
       </div>
     );
