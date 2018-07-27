239b07906adc6c33c3a1691355230064233cce7b
Daniel Munday <mundaymeister@gmail.com>
2018-07-27 12:14:38 +0100

    [meme-generator] v1: changing font size based on text length

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -6,8 +6,12 @@ class App extends Component {
     super(props)
     this.state = {
       memeImage: "https://images.freeimages.com/images/large-previews/7e9/ladybird-1367182.jpg",
-      memeText: "Meme Text",
+      memeText: "Meme Text x",
     }
+  }
+
+  calculateTextSize = (memeText) => {
+    return memeText.length < 10 ? 100 : 10 / memeText.length * 100;
   }
 
   render() {
@@ -16,7 +20,7 @@ class App extends Component {
       <div className="App">
         <div className="meme__container">
           <img className="meme__image" src={memeImage}/>
-          <p class="meme__text">{memeText}</p>
+          <p class="meme__text" style={{fontSize: this.calculateTextSize(memeText)}}>{memeText}</p>
         </div>
       </div>
     );
